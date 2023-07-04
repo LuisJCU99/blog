6d57cbda1e201e8cb26758811068c5fd42ad10c7
luis.castano <luis.castano@thenetfirm.com>
2023-07-04 09:23:48 +0200

    Botones básicos en la página principal

--- resources/views/allPosts.blade.php
+++ resources/views/allPosts.blade.php
@@ -2,25 +2,37 @@
 <!-- component -->
 <section class="bg-white dark:bg-gray-900">
     <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">Blog de Luis Castaño</h1>
+        <div class="flex justify-between items-center mb-4">
+            <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl dark:text-white">Blog de Luis Castaño</h1>
+            <div>
+                <a href="{{ route('dashboard') }}">
+                    <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Dashboard</button>
+                </a>
+                <a href="{{ route('login') }}">
+                    <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Login</button>
+                </a>
+                <a href="{{ route('register') }}">
+                    <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Registrarse</button>
+                </a>
+
+            </div>
+        </div>
 
         <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
             @foreach ($posts as $post)
-                
-            
-            <div class="lg:flex">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="/storage/{{ $post->image_url }}" alt="">
+                <div class="lg:flex">
+                    <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="/storage/{{ $post->image_url }}" alt="">
 
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="{{ route('posts.view',$post->id) }}" class="text-xl font-semibold text-gray-800 hover:underline dark:text-white ">
-                        {{ $post->title }}
-                    </a>
-                    
-                    <span class="text-sm text-gray-500 dark:text-gray-300">En: {{ \Carbon\Carbon::parse($post->created_at)->format('l jS \\of F Y h:i:s A') }}</span>
+                    <div class="flex flex-col justify-between py-6 lg:mx-6">
+                        <a href="{{ route('posts.view',$post->id) }}" class="text-xl font-semibold text-gray-800 hover:underline dark:text-white">
+                            {{ $post->title }}
+                        </a>
+
+                        <span class="text-sm text-gray-500 dark:text-gray-300">En: {{ \Carbon\Carbon::parse($post->created_at)->format('l jS \\of F Y h:i:s A') }}</span>
+                    </div>
                 </div>
-            </div>
             @endforeach
-            
         </div>
     </div>
 </section>
+
